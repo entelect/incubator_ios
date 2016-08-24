95cb21fe65498045ed8d24f48777ba6426d33b24
Keegan Rush <keeganrush@gmail.com>
2016-08-24 18:24:42 +0200

    Write out conversion methods

--- iOSIncubator/iOSIncubator/Util.swift
+++ iOSIncubator/iOSIncubator/Util.swift
@@ -11,11 +11,20 @@ import Foundation
 class Util {
     
     static func numberFromString(string: String) -> NSNumber {
+        let numberFormatter = NumberFormatter()
+        guard let number = numberFormatter.number(from: string) else {
+            return NSNumber(value: 0)
+        }
         
+        return number
     }
     
     static func stringFromDate(date: NSDate) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
         
+        let formattedDateString = dateFormatter.string(from: date as Date)
+        return formattedDateString
     }
     
 }
