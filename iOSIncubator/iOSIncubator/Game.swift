32a07d8783a07da340f67a8c01fa3d57c0fb74d3
Keegan Rush <keeganrush@gmail.com>
2016-08-24 17:55:58 +0200

    Prepare for the start of Module 2
    * Create empty helper methods to convert from a string to an NSNumber and from an NSDate to a string
    * Create Game class

---
+++ iOSIncubator/iOSIncubator/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  iOSIncubator
+//
+//  Created by Keegan Rush on 2016/08/24.
+//  Copyright © 2016 Entelect. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+    var roundNumber = 0
+    let date = NSDate()
+}
---
+++ iOSIncubator/iOSIncubator/Util.swift
@@ -0,0 +1,21 @@
+//
+//  Util.swift
+//  iOSIncubator
+//
+//  Created by Keegan Rush on 2016/08/24.
+//  Copyright © 2016 Entelect. All rights reserved.
+//
+
+import Foundation
+
+class Util {
+    
+    static func numberFromString(string: String) -> NSNumber {
+        
+    }
+    
+    static func stringFromDate(date: NSDate) -> String {
+        
+    }
+    
+}
