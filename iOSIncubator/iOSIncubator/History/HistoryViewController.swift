e0a3c7fa14d45fc470beb6f62a63aa8556c2a8f1
Keegan Rush <keeganrush@gmail.com>
2016-09-07 19:44:29 +0200

    Create HistoryViewController

---
+++ iOSIncubator/iOSIncubator/History/HistoryViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HistoryViewController.swift
+//  iOSIncubator
+//
+//  Created by Keegan Rush on 2016/09/07.
+//  Copyright © 2016 Entelect. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UITableViewController {
+    
+}
