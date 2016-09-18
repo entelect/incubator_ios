6cdb5642a1c0e3153904ac8c770215d5e925d9d7
Keegan Rush <keeganrush@gmail.com>
2016-09-18 11:37:32 +0200

    Populate the layout of the home screen

--- iOSIncubator/iOSIncubator/HomeViewController.swift
+++ iOSIncubator/iOSIncubator/HomeViewController.swift
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playButtonTapped(sender: AnyObject) {
+        print("Play tapped")
+    }
+    
+    @IBAction func viewHistoryButtonTapped(sender: AnyObject) {
+        print("View history tapped")
+    }
 
 }
 
