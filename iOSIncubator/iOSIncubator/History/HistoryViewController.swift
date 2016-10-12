0e67c588e911b1d605980e9c5720be3bf6f2f485
Keegan Rush <keeganrush@gmail.com>
2016-10-12 17:37:12 +0200

    Display history of completed games

--- iOSIncubator/iOSIncubator/History/HistoryViewController.swift
+++ iOSIncubator/iOSIncubator/History/HistoryViewController.swift
@@ -8,11 +8,39 @@
 
 import UIKit
 
+let CellIdentifier = "HistoryTableViewCell"
+
 class HistoryViewController: UITableViewController {
     
+    var games: [Game]!
+    
     override func viewWillAppear(_ animated: Bool) {
-        let games = UserDefaults.standard.array(forKey: UserDefaultsKeys.gameHistory.rawValue) ?? []
+        let gamesData = UserDefaults.standard.array(forKey: UserDefaultsKeys.gameHistory.rawValue)
+        
+        games = gamesData?.flatMap({
+            guard let data = $0 as? Data else { return nil }
+            return NSKeyedUnarchiver.unarchiveObject(with: data) as? Game
+        }) ?? []
+        
         self.title = "\(games.count) games in history"
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let game = games[indexPath.row]
+        
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: CellIdentifier)
+        
+        let winnerText = "\(indexPath.row + 1). Winner: "
+        let appendText = game.winnerType == .human ? "You" : "Computer"
+        cell.textLabel?.text = winnerText + appendText
+        
+        cell.detailTextLabel?.text = game.description
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return games.count
     }
     
 }
