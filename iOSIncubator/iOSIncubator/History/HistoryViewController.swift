bd8399ab912df99c3f944632faa781976e1ab0ef
Keegan Rush <keeganrush@gmail.com>
2016-10-14 05:54:17 +0200

    Print the row number of a UITableView row when it is selected

--- iOSIncubator/iOSIncubator/History/HistoryViewController.swift
+++ iOSIncubator/iOSIncubator/History/HistoryViewController.swift
@@ -47,4 +47,8 @@ class HistoryViewController: UITableViewController {
         return 70
     }
     
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+    }
+    
 }
